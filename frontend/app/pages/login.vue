35c071e155e85868ef236546c58330dd84103e44
yncy0 <yncyncy0@gmail.com>
2026-01-21 15:57:12 +0800

    added: login form from NuxtUI and installed tailwind and zod packages

--- frontend/app/pages/login.vue
+++ frontend/app/pages/login.vue
@@ -1,3 +1,73 @@
+<script setup lang="ts">
+import * as z from "zod";
+import type { FormSubmitEvent, AuthFormField } from "@nuxt/ui";
+
+const toast = useToast();
+
+const fields: AuthFormField[] = [
+  {
+    name: "email",
+    type: "email",
+    label: "Email",
+    placeholder: "Enter your email",
+    required: true,
+  },
+  {
+    name: "password",
+    type: "password",
+    label: "Password",
+    placeholder: "Enter your password",
+    required: true,
+  },
+];
+
+const providers = [
+  {
+    label: "Google",
+    icon: "i-simple-icons-google",
+    onClick: () => {
+      toast.add({ title: "Google", description: "Login with Google" });
+    },
+  },
+  {
+    label: "GitHub",
+    icon: "i-simple-icons-github",
+    onClick: () => {
+      toast.add({ title: "GitHub", description: "Login with GitHub" });
+    },
+  },
+];
+
+const schema = z.object({
+  email: z.email("Invalid email"),
+  password: z
+    .string("Password is required")
+    .min(8, "Must be at least 8 characters"),
+});
+
+type Schema = z.output<typeof schema>;
+
+function onSubmit(payload: FormSubmitEvent<Schema>) {
+  toast.add({
+    title: "Submitted",
+    description: `${payload}`,
+    color: "success",
+  });
+  console.log("Submitted", payload);
+}
+</script>
+
 <template>
-  <h1> This is login </h1>
+  <div class="flex flex-col items-center justify-center gap-4 p-4">
+    <UPageCard class="w-full max-w-md">
+      <UAuthForm
+        :schema="schema"
+        title="Login"
+        description="Enter your credentials to access your account."
+        :fields="fields"
+        :providers="providers"
+        @submit="onSubmit"
+      />
+    </UPageCard>
+  </div>
 </template>
--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/hints', '@nuxt/ui']
-})
+  modules: ["@nuxt/eslint", "@nuxt/hints", "@nuxt/ui"],
+
+  css: ["./app/assets/css/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
